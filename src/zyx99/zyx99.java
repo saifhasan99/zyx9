7e6ea92658860820d945cb5665ec6c9030d1dda3
saifhasan99 <saifhasan080@gmail.com>
2023-01-09 01:35:12 -0800

    update

---
+++ src/zyx99/zyx99.java
@@ -0,0 +1,21 @@
+package zyx99;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class zyx99 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+
+		WebDriverManager.chromedriver().setup();
+
+		driver = new ChromeDriver();
+
+		driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+	}
+
+}
